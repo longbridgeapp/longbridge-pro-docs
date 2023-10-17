156b8bc39176d645162ece7e076c6ce0cbba0967
阮文涛 <wentao.ruan@longbridge.sg>
2023-10-17 14:58:18 +0800

    fix: update tranlate script

--- scripts/cn2hk.js
+++ scripts/cn2hk.js
@@ -1,7 +1,7 @@
 // Iter *.md in docs/zh-CN/**/*.md and use OpenCC to convert to docs/zh-HK/**/*.md
 // Usage: node scripts/cn2hk.js
 
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 const glob = require('glob');
 const OpenCC = require('opencc-js');
@@ -12,18 +12,31 @@ const converter = OpenCC.Converter({ from: 'cn', to: 'hk' });
  * Iterator all .md file in docs/zh-CN/ and convert to docs/zh-HK/
  */
 const convert = () => {
+
   let content = fs.readFileSync('docs/zh-CN.md', 'utf8');
   let newContent = converter(content);
   fs.writeFileSync('docs/zh-HK.md', newContent);
 
-  let files = glob.sync('docs/zh-CN/**/*.md');
+
+  const sourceFolder = 'docs/zh-CN'; // 源文件夹
+  const destinationFolder = 'docs/zh-HK'; // 目标文件夹 
+  fs.ensureDirSync(destinationFolder);
+  const files = fs.readdirSync(sourceFolder)
   files.forEach((file) => {
+    const sourcePath = `${sourceFolder}/${file}`;
+    const destinationPath = `${destinationFolder}/${file}`;
+    // 复制文件
+    fs.copySync(sourcePath, destinationPath);
+    console.log(`复制文件：${file}`);
+  })
+  console.log(`所有文件从 ${sourceFolder} 到 ${destinationFolder} 复制完成。`);
+
+
+  let filesHK = glob.sync('docs/zh-HK/**/*.md');
+  filesHK.forEach((file) => {
     let content = fs.readFileSync(file, 'utf8');
     let newContent = converter(content);
-    let newFile = file.replace('zh-CN', 'zh-HK');
-    let folder = path.dirname(newFile);
-    fs.mkdirSync(folder, { recursive: true });
-    fs.writeFileSync(newFile, newContent);
+    fs.writeFileSync(file, newContent, 'utf8');
   });
 };
 
